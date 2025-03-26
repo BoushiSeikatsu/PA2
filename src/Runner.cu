0e8e7180f6b6d3184f31e3e214e1060a0a96bdb4
Duba Michal <dub0074@vsb.cz>
2025-03-26 14:50:00 +0100

    6th class

--- src/Runner.cu
+++ src/Runner.cu
@@ -1,278 +1,151 @@
-﻿#include <cudaDefs.h>
-#include <random>
-#include <algorithm>
-#include <iostream>
+// includes, cudaimageWidth
 #include <cudaDefs.h>
-#include <time.h>
-#include <math.h>
 
-using std::cout;
-using std::endl;
+#include <helper_math.h>			// normalize method
+
+#include <imageManager.h>
+#include <imageUtils.cuh>
+#include <benchmark.h>
+
+
+#define TPB_1D 8						// ThreadsPerBlock in one dimension
+#define TPB_2D TPB_1D*TPB_1D			// ThreadsPerBlock = TPB_1D*TPB_1D (2D block)
 
 cudaError_t error = cudaSuccess;
 cudaDeviceProp deviceProp = cudaDeviceProp();
+__constant__ int dfilter_x[3][3];
+__constant__ int dfilter_y[3][3];
+using DT = float;
 
-constexpr unsigned int VECTOR_SIZE = 3;
-constexpr unsigned int THREAD_COUNT = 256;
-
-__constant__ __device__ float dMinFloat;
-__constant__ __device__ float dMaxFloat;
-//__constant__ __device__ int zeroVector[VECTOR_SIZE] = { 0,0,0 };//For now I consider only 3D vectors for simplicity -> Nebude vlastně třeba jelikož to je 0,0,0
-
-
-__host__ float* createData(const unsigned int length)
+__host__ TextureInfo createTextureObjectFrom2DArray(const ImageInfo<DT>& ii)
 {
-	// Random number generator setup
-	std::random_device rd;
-	std::mt19937_64 mt(rd());
-	// Trying to simulate unknown bounds
-	std::uniform_real_distribution<float> lowerBound(0.1f, 1.0f);
-	std::uniform_real_distribution<float> upperBound(2.0f, 3.0f);
-	float lowerBoundValue = lowerBound(mt);
-	float upperBoundValue = upperBound(mt);
-	std::uniform_real_distribution<float> dist(lowerBoundValue, upperBoundValue);
+	TextureInfo ti;
 
-	// Allocate memory for float array
-	float* data;
-	cudaMallocManaged(&data, length * sizeof(float));
+	// Size info
+	ti.size = { ii.width, ii.height, 1 };
+	//Texture Data settings
+	ti.texChannelDesc = cudaCreateChannelDesc<DT>();  // cudaCreateChannelDesc(32, 0, 0, 0, cudaChannelFormatKindUnsigned);
+	checkCudaErrors(cudaMallocArray(&ti.texArrayData, &ti.texChannelDesc, ii.width, ii.height));
+	checkCudaErrors(cudaMemcpyToArray(ti.texArrayData, 0, 0, ii.dPtr, ii.pitch * ii.height, cudaMemcpyDeviceToDevice));
 
-	if (!data) {
-		fprintf(stderr, "Memory allocation failed!\n");
-		return nullptr;
-	}
+	// Specify texture resource
+	ti.resDesc.resType = cudaResourceTypeArray;
+	ti.resDesc.res.array.array = ti.texArrayData;
 
-	// Populate the array with random float values
-	for (unsigned int i = 0; i < length; i++) {
-		data[i] = dist(mt);
-	}
+	// Specify texture object parameters
+	ti.texDesc.addressMode[0] = cudaAddressModeClamp;
+	ti.texDesc.addressMode[1] = cudaAddressModeClamp;
+	ti.texDesc.filterMode = cudaFilterModePoint;
+	ti.texDesc.readMode = cudaReadModeElementType;
+	ti.texDesc.normalizedCoords = false;
 
-	// Ensure memory is synchronized if using unified memory
-	cudaDeviceSynchronize();
+	// Create texture object
+	checkCudaErrors(cudaCreateTextureObject(&ti.texObj, &ti.resDesc, &ti.texDesc, NULL));
 
-	return data;
+	return ti;
+
 }
-/*
-Zjisti min a max hodnoty v matici
-*/
-__global__ void kernelFindMinMax(const float* __restrict__ matrix, const size_t m_size, const size_t n_size, float* __restrict__ minFloat, float* __restrict__ maxFloat)
+
+template<bool normalizeTexel>__global__ void createNormalmap(const cudaTextureObject_t srcTex, const unsigned int srcWidth, const unsigned int srcHeight, const unsigned int dstPitchInElements, uchar3* dst)
 {
-	__shared__ float sMin[THREAD_COUNT];
-	__shared__ float sMax[THREAD_COUNT];
-	//unsigned int offset_x = threadIdx.x + blockIdx.x * blockDim.x;
-	//unsigned int offset_y = threadIdx.y + blockIdx.y * blockDim.y;
-	unsigned int offset = threadIdx.x + blockIdx.x * blockDim.x;
-	if (offset < m_size* n_size) // Here I check boundaries of matrix
+	//TODO
+	const unsigned int offset_x = threadIdx.x + blockIdx.x * blockDim.x;
+	const unsigned int offset_y = threadIdx.y + blockIdx.y * blockDim.y;
+    unsigned int position = offset_y * dstPitchInElements + offset_x;
+	int sum_x = 0;
+	int sum_y = 0;
+	if ((offset_x >= srcWidth) || (offset_y >= srcHeight)) return;
+	if (offset_y - 1 > 0)
 	{
-		sMin[threadIdx.x] = matrix[offset];
-		sMax[threadIdx.x] = matrix[offset];
-	}
-	else
-	{
-		sMin[threadIdx.x] = FLT_MAX;
-		sMax[threadIdx.x] = FLT_MIN;
-	}
-	__syncthreads();
-	for (int i = blockDim.x / 2; i > 0; i >>= 1)
-	{
-		if (threadIdx.x < i)
+		if (offset_x - 1 > 0)
 		{
-			sMin[threadIdx.x] = fminf(sMin[threadIdx.x], sMin[threadIdx.x + i]);
-			sMax[threadIdx.x] = fmaxf(sMax[threadIdx.x], sMax[threadIdx.x + i]);
+			sum_x += tex2D<float>(srcTex, offset_x - 1, offset_y - 1) * dfilter_x[0][0];
+			sum_y += tex2D<float>(srcTex, offset_x - 1, offset_y - 1) * dfilter_y[0][0];
+
 		}
-		__syncthreads();
+		sum_x += tex2D<float>(srcTex, offset_x, offset_y - 1) * dfilter_x[0][1];
+		sum_y += tex2D<float>(srcTex, offset_x, offset_y - 1) * dfilter_y[0][1];
+		sum_x += tex2D<float>(srcTex, offset_x + 1, offset_y - 1) * dfilter_x[0][2];
+		sum_y += tex2D<float>(srcTex, offset_x + 1, offset_y - 1) * dfilter_y[0][2];
 	}
-	if (threadIdx.x == 0)
+	if (offset_x - 1 > 0)
 	{
-		if (sMin[0] < *minFloat)
-			*minFloat = sMin[0];
-		if (sMax[0] > *maxFloat)
-			*maxFloat = sMax[0];
+		sum_x += tex2D<float>(srcTex, offset_x - 1, offset_y) * dfilter_x[1][0];
+		sum_y += tex2D<float>(srcTex, offset_x - 1, offset_y) * dfilter_y[1][0];
+		sum_x += tex2D<float>(srcTex, offset_x - 1, offset_y + 1) * dfilter_x[2][0];
+		sum_y += tex2D<float>(srcTex, offset_x - 1, offset_y + 1) * dfilter_y[2][0];
 	}
-}
-__global__ void kernelDiscretize(const float* __restrict__ matrix, const size_t m_size, const size_t n_size, uint8_t* __restrict__ dMatrix)
-{
-	//int offset_x = threadIdx.x + blockIdx.x * blockDim.x;
-	//int offset_y = threadIdx.y + blockIdx.y * blockDim.y;
-	int offset = threadIdx.x + blockIdx.x * blockDim.x;
-	if (offset < m_size * n_size) // Here I check boundaries of matrix
+	sum_x += tex2D<float>(srcTex, offset_x, offset_y + 1) * dfilter_x[2][1];
+	sum_y += tex2D<float>(srcTex, offset_x, offset_y + 1) * dfilter_y[2][1];
+	sum_x += tex2D<float>(srcTex, offset_x + 1, offset_y) * dfilter_x[1][2];
+	sum_y += tex2D<float>(srcTex, offset_x + 1, offset_y) * dfilter_y[1][2];
+	sum_x += tex2D<float>(srcTex, offset_x + 1, offset_y + 1) * dfilter_x[2][2];
+	sum_y += tex2D<float>(srcTex, offset_x + 1, offset_y + 1) * dfilter_y[2][2];
+	float3 d;
+	d.x = sum_x;
+	d.y = sum_y;
+	d.z = 1.f/2.f;
+	if (normalizeTexel)
 	{
-		float value = matrix[offset];
-		float normalized_value = (value - dMinFloat) / (dMaxFloat - dMinFloat);
-		dMatrix[offset] = static_cast<uint8_t>(normalized_value * 255); // Discretize to uint8_t
+		d = normalize(d);
 	}
-}
-// One thread = one vector
-__global__ void kernelGetDistances(const uint8_t* __restrict__ matrix, const size_t m_size, const size_t n_size, float* origin, float* __restrict__ distances)
-{
-	unsigned int offset_x = threadIdx.x + blockIdx.x * blockDim.x;
-	if (offset_x < m_size) // Here I check boundaries of matrix
-	{
-		distances[offset_x] = 0.0f;
-
-		// Compute squared Euclidean distance for the vector
-		for (unsigned int i = 0; i < n_size; i++) {
-			uint8_t value = matrix[offset_x + i];
-			uint8_t originValue = origin[i];
-			distances[offset_x] += (value - originValue) * (value - originValue);
-		}
-	}
+	d.x = (d.x + 1) * 127.5;
+	d.y = (d.y + 1) * 127.5;
+	d.z = (d.z + 1) * 127.5;
+	dst[position] = make_uchar3(static_cast<uint8_t>(d.z), static_cast<uint8_t>(d.y), static_cast<uint8_t>(d.x));
 }
 
-__global__ void kernelGetFarthest(float* __restrict__ distances, const size_t m_size, const size_t n_size, int* __restrict__ vectorIndex)
+void saveTexImage(const char* imageFileName, const uint32_t dstWidth, const uint32_t dstHeight, const uint32_t dstPitch, const uchar3* dstData)
 {
-	unsigned int offset = threadIdx.x + blockIdx.x * blockDim.x;
-
-	// Ensure we're within bounds
-	if (offset < m_size)
-	{
-		// Get the current value of the farthest index (from vectorIndex[0])
-		int currentIndex = vectorIndex[0];
-
-		// Use atomicCAS to safely update the vectorIndex if the current distance is greater than the one at the current index
-		// We only want to update vectorIndex[0] if distances[offset] > distances[currentIndex]
-		while (distances[offset] > distances[currentIndex])
-		{
-			// Perform atomicCAS to check if vectorIndex[0] matches the currentIndex
-			// If it does, we update it with the current offset; otherwise, we continue trying.
-			currentIndex = atomicCAS(&vectorIndex[0], currentIndex, offset);
-
-			// If currentIndex didn't match, it means another thread updated vectorIndex[0], so we need to re-check the condition
-			if (currentIndex == vectorIndex[0]) {
-				break;  // Exit the loop if no update was made
-			}
-		}
-	}
+	FIBITMAP* tmp = FreeImage_Allocate(dstWidth, dstHeight, 24);
+	unsigned int tmpPitch = FreeImage_GetPitch(tmp);					// FREEIMAGE align row data ... You have to use pitch instead of width
+	checkCudaErrors(cudaMemcpy2D(FreeImage_GetBits(tmp), tmpPitch, dstData, dstPitch, dstWidth * 3, dstHeight, cudaMemcpyDeviceToHost));
+	//FreeImage_Save(FIF_BMP, tmp, imageFileName, 0);
+	ImageManager::GenericWriter(tmp, imageFileName, FIF_BMP);
+	FreeImage_Unload(tmp);
 }
 
-int main(int argc, char* argv[])
+__global__ void texKernel(const cudaTextureObject_t srcTex, const unsigned int srcWidth, const unsigned int srcHeight, float* dst)
 {
-	//Inicializace a alokace promennych
+	int offset_x = threadIdx.x + blockIdx.x * blockDim.x;
+	int offset_y = threadIdx.y + blockIdx.y * blockDim.y;
+	if ((offset_x >= srcWidth) || (offset_y >= srcHeight)) return;
+	dst[srcWidth * offset_y + offset_x] = tex2D<float>(srcTex, offset_x, offset_y);
+}
 
+
+int main(int argc, char *argv[])
+{
 	initializeCUDA(deviceProp);
-	cudaEvent_t startEvent, stopEvent;
-	float elapsedTime;
-	unsigned int m_size = 2 << 20;
-	unsigned int n_size = VECTOR_SIZE;
-	unsigned int threadCount = THREAD_COUNT;
-	unsigned int blockCount = (m_size * n_size + threadCount - 1) / threadCount;
-	uint8_t* dMatrix = nullptr;
-	checkCudaErrors(cudaMallocManaged(&dMatrix, m_size * n_size * sizeof(uint8_t)));
-	float* minFloat = nullptr;
-	float* maxFloat = nullptr;
-	checkCudaErrors(cudaMallocManaged(&minFloat, sizeof(float)));
-	checkCudaErrors(cudaMallocManaged(&maxFloat, sizeof(float)));
+	FreeImage_Initialise();
+	int filter_x[3][3] = { { -1, 0, 1 },{ -2, 0, 2 },{ -1, 0, 1 } };
+	int filter_y[3][3] = { { -1, -2, -1 },{ 0, 0, 0 },{ 1, 2, 1 } };
+	cudaMemcpyToSymbol(dfilter_x, filter_x, 3 * 3 * sizeof(int));
+	cudaMemcpyToSymbol(dfilter_y, filter_y, 3 * 3 * sizeof(int));
+	// STEP 1 - load raw image data, HOST->DEVICE, with/without pitch
+	ImageInfo<DT> src;
+	prepareData<false>("C:\\Users\\dub0074\\Documents\\PA2\\src/terrain3Kx3K.tif", src);
 
-	// Nastaveni default hodnot
-	*minFloat = FLT_MAX;
-	*maxFloat = FLT_MIN;
-	float* hMatrix = createData(m_size * n_size);
-	cudaEventCreate(&startEvent);
-	cudaEventCreate(&stopEvent);
-	cudaEventRecord(startEvent, 0);
-	kernelFindMinMax << <blockCount, threadCount >> > (hMatrix, m_size, n_size, minFloat, maxFloat);
-	checkCudaErrors(cudaDeviceSynchronize());  // Wait for kernel to finish
-	cudaEventRecord(stopEvent, 0);
-	cudaEventSynchronize(stopEvent);
-
-	cudaEventElapsedTime(&elapsedTime, startEvent, stopEvent);
-	printf("Finding MinMax Float: %f ms\n", elapsedTime);
-	cudaEventRecord(startEvent, 0);
-	cudaEventSynchronize(startEvent);
-	//Ted zname min a max a muzeme je nahrat do constant memory pro pouziti k diskretizaci
-	checkCudaErrors(cudaMemcpyToSymbol(dMinFloat, minFloat, sizeof(float)));
-	checkCudaErrors(cudaMemcpyToSymbol(dMaxFloat, maxFloat, sizeof(float)));
-	printf("Min: %f, Max: %f\n", *minFloat, *maxFloat);
+	// STEP 2 - create texture from the raw data
+	TextureInfo tiSrc = createTextureObjectFrom2DArray(src);
+	// SETP 3 - allocate pitch memory to store output image data
 	
-	kernelDiscretize << <blockCount, threadCount >> > (hMatrix, m_size, n_size, dMatrix);
-	checkCudaErrors(cudaDeviceSynchronize());  // Wait for kernel to finish
-	cudaEventRecord(stopEvent, 0);
-	cudaEventSynchronize(stopEvent);
+	size_t dstPitch;
+	uchar3* dst = 0;
+	cudaMallocPitch((void**)&dst, &dstPitch, src.width * sizeof(uchar3), src.height);
+	// STEP 4 - create normal map
+	dim3 block = { TPB_1D, TPB_1D,1 };
+	dim3 grid{ (src.width + TPB_1D - 1) / TPB_1D, (src.height + TPB_1D - 1) / TPB_1D,  1 };
+	createNormalmap<true> << <grid, block >> > (tiSrc.texObj, src.width, src.height, dstPitch / sizeof(uchar3), dst);
+	// STEP 5 - save the normal map
+	saveTexImage("C:\\Users\\dub0074\\Documents\\PA2\\src/normalmap.bmp", src.width, src.height, dstPitch, dst);
+	// SETP 6 - release unused data
+	if (tiSrc.texObj) checkCudaErrors(cudaDestroyTextureObject(tiSrc.texObj));
+	if (tiSrc.texArrayData) checkCudaErrors(cudaFreeArray(tiSrc.texArrayData));
+	//if (src.) cudaFree(src.dPtr);
+	if (dst) cudaFree(dst);
+	cudaDeviceSynchronize();
+	error = cudaGetLastError();
 
-	cudaEventElapsedTime(&elapsedTime, startEvent, stopEvent);
-	printf("Discretization: %f ms\n", elapsedTime);
-	cudaEventRecord(startEvent, 0);
-	cudaEventSynchronize(startEvent);
-	float* dDistances = nullptr;
-	checkCudaErrors(cudaMallocManaged(&dDistances, sizeof(float) * m_size));
-	float* origin = nullptr;
-	checkCudaErrors(cudaMallocManaged(&origin, sizeof(float) * n_size));
-	for (int i = 0; i < n_size; i++)
-	{
-		origin[i] = 0.0f;
-	}
-	kernelGetDistances << <(m_size + threadCount - 1) / threadCount, threadCount >> > (dMatrix, m_size, n_size, origin, dDistances); // One thread = one vector
-	checkCudaErrors(cudaDeviceSynchronize());  // Wait for kernel to finish
-	cudaEventRecord(stopEvent, 0);
-	cudaEventSynchronize(stopEvent);
-
-	cudaEventElapsedTime(&elapsedTime, startEvent, stopEvent);
-	printf("Getting distances from zero vector: %f ms\n", elapsedTime);
-	cudaEventRecord(startEvent, 0);
-	cudaEventSynchronize(startEvent);
-
-	/*for (int i = 0; i < 10; i++)
-	{
-		printf("Distance %d: %f\n", i, dDistances[i]);
-	}*/
-
-	int* vectorIndex = nullptr;
-	checkCudaErrors(cudaMallocManaged(&vectorIndex, sizeof(int)));
-	*vectorIndex = 0;
-	kernelGetFarthest << <(m_size + threadCount - 1) / threadCount, threadCount >> > (dDistances, m_size, n_size, vectorIndex);
-	checkCudaErrors(cudaDeviceSynchronize());  // Wait for kernel to finish
-	cudaEventRecord(stopEvent, 0);
-	cudaEventSynchronize(stopEvent);
-
-	cudaEventElapsedTime(&elapsedTime, startEvent, stopEvent);
-	printf("Finding farthest vector: %f ms\n", elapsedTime);
-
-	printf("Farthest vector index: %d\n", *vectorIndex);
-	printf("Farthest vector distance: %f\n", dDistances[*vectorIndex]);
-	printf("Farthest vector: ");
-	for (int i = 0; i < n_size; i++)
-	{
-		printf("%d ", dMatrix[n_size * *vectorIndex + i]);
-	}
-	printf("\n");
-	for (int i = 0; i < n_size; i++)
-	{
-		origin[i] = dMatrix[n_size * *vectorIndex + i]; //Set furthest vector as origin
-	}
-	kernelGetDistances << <(m_size + threadCount - 1) / threadCount, threadCount >> > (dMatrix, m_size, n_size, origin, dDistances); // One thread = one vector
-	checkCudaErrors(cudaDeviceSynchronize());  // Wait for kernel to finish
-	cudaEventRecord(stopEvent, 0);
-	cudaEventSynchronize(stopEvent);
-
-	cudaEventElapsedTime(&elapsedTime, startEvent, stopEvent);
-	printf("Getting distances from farthest vector: %f ms\n", elapsedTime);
-
-	*vectorIndex = 0;
-	kernelGetFarthest << <(m_size + threadCount - 1) / threadCount, threadCount >> > (dDistances, m_size, n_size, vectorIndex);
-	checkCudaErrors(cudaDeviceSynchronize());  // Wait for kernel to finish
-	cudaEventRecord(stopEvent, 0);
-	cudaEventSynchronize(stopEvent);
-
-	cudaEventElapsedTime(&elapsedTime, startEvent, stopEvent);
-	printf("Finding farthest vector: %f ms\n", elapsedTime);
-
-	printf("Farthest vector distance: %f\n", dDistances[*vectorIndex]);
-	printf("Farthest vector: ");
-	for (int i = 0; i < n_size; i++)
-	{
-		printf("%d ", dMatrix[n_size * *vectorIndex + i]);
-	}
-	printf("\n");
-	/*if (hMatrix)
-		free(hMatrix);*/
-
-	checkCudaErrors(cudaFree(minFloat));
-	checkCudaErrors(cudaFree(maxFloat));
-	checkCudaErrors(cudaFree(dMatrix));
-	checkCudaErrors(cudaFree(dDistances));
-	checkCudaErrors(cudaFree(vectorIndex));
-	checkCudaErrors(cudaFree(origin));
-	cudaEventDestroy(startEvent);
-	cudaEventDestroy(stopEvent);
-
-	
+	FreeImage_DeInitialise();
 }
---
+++ src/tasks.md
@@ -0,0 +1,9 @@
+# Parallel Algorithms 2 - Practical Lessons
+## LESSON 6
+### Prerequisites
+* CUDA - unified memory (Unified Virtual Addressing)
+* Texture Object API
+* [Download an input image (3Kx3K terrain heightmap)](https://vsb.sharepoint.com/:i:/r/sites/PAII/Sdilene%20dokumenty/General/Practical%20Lessons/06/terrain3Kx3K.tif?csf=1&web=1&e=Mdll8x)
+### Topics and Tasks
+Load the heightmap and prepare a texture using CUDA Texture Object API
+Create a normal map from the input image using a Sobel operator
